ee4f1b7642f4652dffae7750079f4cca2885e78f
shubham lohani <shubham lohani@INBOOK_X1_NEO>
2024-03-11 17:45:43 +0530

    First Commit

---
+++ src/optional5/jtc.java
@@ -0,0 +1,20 @@
+
+package optional5;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public class jtc {
+ public static void main(String[] args) {
+	String s1="som";
+	Optional<Optional<String>> op1=Optional.of(Optional.ofNullable(s1));
+    System.out.println(op1);
+    System.out.println("------------------");
+    System.out.println(op1.map(input->input));
+    System.out.println(op1.flatMap(input->input));
+    System.out.println("=============");
+    Optional<Optional<String>> op2=op1.map(input1->input1.map(input2->input2.toUpperCase()));
+    System.out.println(op2);
+    
+ }
+}
